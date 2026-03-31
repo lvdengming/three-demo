b960fe02a0eefeb7762170d1216a7ddd103239cb
lvdengming <lvdengming@foxmail.com>
2026-04-01 07:51:50 +0800

    feat: 初始化 threejs 资源

---
+++ src/cubic/World/index.js
@@ -0,0 +1,7 @@
+/*
+ * @Author: lvdengming@foxmail.com
+ * @Date: 2026-04-01 07:44:34
+ * @LastEditors: lvdengming@foxmail.com
+ * @LastEditTime: 2026-04-01 07:44:53
+ */
+export class World {}
---
+++ src/cubic/main.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: lvdengming@foxmail.com
+ * @Date: 2026-04-01 07:40:02
+ * @LastEditors: lvdengming@foxmail.com
+ * @LastEditTime: 2026-04-01 07:45:43
+ */
+async function main() {
+  const container = document.getElementById('scene-container');
+}
+
+main().catch(console.error);
